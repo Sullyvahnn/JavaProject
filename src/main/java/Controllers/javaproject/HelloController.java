9d43036b23f5ddeba5039c7646e268bbd328078c
Sullyvahnn <wojnar.artur1@gmail.com>
2025-01-12 16:35:00 +0100

    corrected some minor errors in project structure

--- src/main/java/Controllers/javaproject/HelloController.java
+++ src/main/java/Controllers/javaproject/HelloController.java
@@ -1,5 +1,6 @@
 package Controllers.javaproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -10,5 +11,15 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onBuyerButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onSellerButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onAdminButtonClick(ActionEvent actionEvent) {
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.javaproject {
     requires javafx.controls;
     requires javafx.fxml;
-
+    opens Controllers.javaproject to javafx.fxml;
 
     opens com.example.javaproject to javafx.fxml;
     exports com.example.javaproject;
